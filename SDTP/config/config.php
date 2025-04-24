e74bae55e38e19d20705a35ca8d29ef517951bd9
mishVN <hmvniroshan@students.nsbm.ac.lk>
2025-04-24 23:20:55 +0530

    Create config.php
    create database connection

---
+++ SDTP/config/config.php
@@ -0,0 +1,12 @@
+<?php
+$host ='localhost';
+$user = 'root';
+$pass = '3323';
+$db = 'air_quality';
+
+$conn = new mysql($host,$user,$pass,$db);
+
+if($conn->connect_error){
+    die(json_encode(["error" => "Database connection failed", "details" => $conn->connect_error]));
+}
+?>
