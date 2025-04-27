8cce212239cb11e567fb9d2c4794b37d23013eb8
whshsamaranayaka <whshsamaranayaka@students.nsbm.ac.lk>
2025-04-27 22:45:11 +0530

    Create notification.php
    Create UI

---
+++ SDTP/notification.php
@@ -0,0 +1,161 @@
+<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="./css/login.css" />
+    <script src="https://kit.fontawesome.com/857ecf43e2.js" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.css" />
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <title>Air Quality Monitoring Dashboard</title>
+</head>
+
+<body>
+    <nav>
+        <div class="logo-name">
+            <div class="logo-image">
+                <img src="./images/air-quality-96.png" alt="logo" />
+            </div>
+
+            <span class="logo_name">Air Quality Monitoring</span>
+        </div>
+
+        <div class="menu-items">
+            <ul class="nav-links">
+                <li>
+                    <a href="./index.php">
+                        <i class="fa-solid fa-house"></i>
+                        <span class="link-name">Dahsboard</span>
+                    </a>
+                </li>
+               
+                <li>
+                    <a href="./sensors.php">
+                        <i class="fa-solid fa-tablet-button"></i>
+                        <span class="link-name">Sensors</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="./data_simulate.php">
+                        <i class="fa-solid fa-chart-simple"></i>
+                        <span class="link-name">Data Simulate</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="./user_manage.php">
+                        <i class="fa-solid fa-users-gear"></i>
+                        <span class="link-name">User Manage</span>
+                    </a>
+                </li>
+
+                
+                <li class="active">
+                    <a href="./notifications.php">
+                        <i class="fa-solid fa-bell"></i>
+                        <span class="link-name">Notification</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="./help.php">
+                        <i class="fa-solid fa-circle-info"></i>
+                        <span class="link-name">Help</span>
+                    </a>
+                </li>
+
+            </ul>
+
+            <ul class="logout-mode">
+                <li>
+                    <a href="./components/logout.php">
+                        <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                        <span class="link-name">Logout</span>
+                    </a>
+                </li>
+
+
+                <li class="mode">
+                    <a href="#">
+                        <i class="fa-solid fa-moon"></i>
+                        <span class="link-name">Dark Mode</span>
+                    </a>
+
+                    <div class="mode-toggle">
+                        <span class="switch"></span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <section class="dashboard">
+        <div class="top">
+            <i class="fa-solid fa-bars sidebar-toggle"></i>
+
+            <div class=" search-box">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <input type="text" placeholder="Search here..." />
+            </div>
+
+           
+            <img src="./images/Profile-PNG-Photo.png" alt="" />
+            
+
+            <div class="admin-login">
+                <a href="./components/login.php">
+                    <span class="link-name">Admin</span>
+                </a>
+            </div>
+        </div>
+
+        <div class="table-wrapper">
+
+            <div class="new-sensor-forms">
+
+                <h2 style="padding: 30px 40px 0px 40px;">AQI Alert Configuration</h2>
+
+                <form method="POST" action="" style="padding: 40px; width: 40%;">
+                    <div class="input-group">
+                        <label>AQI Threshold for Moderate</label>
+                        <input type="number" name="moderate" value="<?php echo $alert_settings['moderate']; ?>"
+                            required>
+                    </div>
+                    <div class="input-group">
+                        <label>AQI Threshold for Unhealthy</label>
+                        <input type="number" name="unhealthy" value="<?php echo $alert_settings['unhealthy']; ?>"
+                            required>
+                    </div>
+                    <div class="input-group">
+                        <label>AQI Threshold for Very Unhealthy</label>
+                        <input type="number" name="very_unhealthy"
+                            value="<?php echo $alert_settings['very_unhealthy']; ?>" required>
+                    </div>
+                    <div class="input-group">
+                        <label>AQI Threshold for Hazardous</label>
+                        <input type="number" name="hazardous" value="<?php echo $alert_settings['hazardous']; ?>"
+                            required>
+                    </div>
+                    <div class="input-group check">
+                        <label>Enable Alerts</label>
+                        <input type="checkbox" name="enable_alerts"
+                            <?php echo $alert_settings['enable_alerts'] ? 'checked' : ''; ?>>
+                    </div>
+                    <button type="submit" class="btn" name="update_alerts">Update Alerts</button>
+                </form>
+            </div>
+        </div>
+    </section>
+   
+</body>
+
+</html>
+?>
