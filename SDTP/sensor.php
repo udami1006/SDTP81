4afc583ef934e55b6592ed59e3e722bd65f30714
PLSamarasingha <Samarasinghapramod@gmail.com>
2025-04-28 14:40:14 +0530

    Update sensor.php
    Back end create.

--- SDTP/sensor.php
+++ SDTP/sensor.php
@@ -1,4 +1,46 @@
 <?php
+session_start();
+include './config/config.php';
+
+if (!isset($_SESSION['admin'])) {
+    header("Location: index.php");
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['insert'])) {
+        $name = $_POST['name'];
+        $lat = $_POST['lat'];
+        $lng = $_POST['lng'];
+        $status = $_POST['status'];
+
+        $stmt = $conn->prepare("INSERT INTO sensors (name, lat, lng, status) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $name, $lat, $lng, $status);
+        if ($stmt->execute()) {
+            header("Location: sensors.php");
+            exit();
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+    }
+
+    if (isset($_POST['delete'])) {
+        $sensor_id = $_POST['sensor_id'];
+
+        $stmt = $conn->prepare("DELETE FROM sensors WHERE sensor_id = ?");
+        $stmt->bind_param("i", $sensor_id);
+        if ($stmt->execute()) {
+            echo "<script>alert('Sensor deleted successfully!');</script>";
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+    }
+}
+
+$sql = "SELECT * FROM sensors";
+$result = $conn->query($sql);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,6 +48,14 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="./css/login.css" />
+    <script src="https://kit.fontawesome.com/857ecf43e2.js" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.css" />
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -30,6 +80,9 @@
                         <span class="link-name">Dahsboard</span>
                     </a>
                 </li>
+                <?php
+                  if (isset($_SESSION['admin'])) { 
+                ?>
                 <li class="active">
                     <a href="./sensors.php">
                         <i class="fa-solid fa-tablet-button"></i>
@@ -48,6 +101,10 @@
                         <span class="link-name">User Manage</span>
                     </a>
                 </li>
+
+                <?php 
+                  }
+                ?>
                 <li>
                     <a href="./notifications.php">
                         <i class="fa-solid fa-bell"></i>
@@ -95,11 +152,23 @@
                 <input type="text" placeholder="Search here..." />
             </div>
 
+            <?php
+              if (isset($_SESSION['admin'])) { 
+            ?>
+            <img src="./images/Profile-PNG-Photo.png" alt="" />
+            <?php 
+              }else{
+            ?>
+
             <div class="admin-login">
                 <a href="./components/login.php">
                     <span class="link-name">Admin</span>
                 </a>
             </div>
+
+            <?php
+              }
+            ?>
 
         </div>
 
@@ -120,12 +189,13 @@
                         </tr>
                     </thead>
                     <tbody>
+                        <?php while ($row = $result->fetch_assoc()) { ?>
                         <tr>
-                            <td>1001</td>
-                            <td>Colomo</td>
-                            <td>6.012546</td>
-                            <td>8.326598</td>
-                            <td>Active</td>
+                            <td><?php echo $row['sensor_id']; ?></td>
+                            <td><?php echo $row['name']; ?></td>
+                            <td><?php echo $row['lat']; ?></td>
+                            <td><?php echo $row['lng']; ?></td>
+                            <td><?php echo $row['status']; ?></td>
                             <td>
                                 <!-- Form for Delete with confirmation -->
                                 <form action="" method="POST" onsubmit="return confirmDelete()">
@@ -136,6 +206,7 @@
                                 </form>
                             </td>
                         </tr>
+                        <?php } ?>
                     </tbody>
                 </table>
             </div>
@@ -171,8 +242,17 @@
             </div>
         </div>
     </section>
+
+    <script>
+    function confirmDelete() {
+        return confirm("Are you sure you want to delete this sensor?");
+    }
+    </script>
+    <script src="./js/actions.js"></script>
 </body>
 
 </html>
 
+<?php
+$conn->close();
 ?>
